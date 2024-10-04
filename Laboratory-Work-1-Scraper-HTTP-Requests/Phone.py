ab048af9f84483cf7178278570b3f8e080d9ced5
Ghenntoggy1 <roman.gusev@isa.utm.md>
2024-10-04 15:44:06 +0300

    feature: Add PhoneEntity class that will store phone details.

---
+++ Laboratory-Work-1-Scraper-HTTP-Requests/Phone.py
@@ -0,0 +1,13 @@
+import re
+
+
+class PhoneEntity:
+    def __init__(self, url: str, title: str, price: dict, description: str):
+        self.url = url
+        self.title = title
+        self.price = price
+        # POINT
+        self.description = re.sub(" : ", ":", description)
+
+    def __repr__(self):
+        return f"PhoneEntity({self.url}, {self.title}, {self.price}, {self.description})"
