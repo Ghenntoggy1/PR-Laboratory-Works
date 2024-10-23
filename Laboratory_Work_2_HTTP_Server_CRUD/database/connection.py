a74b0e4e2f95952f0cc2e915195f88425dd98c4f
Ghenntoggy1 <roman.gusev@isa.utm.md>
2024-10-24 00:36:31 +0300

    feature: Add connection function to connect to Database. Modify Models folder into schemas - contains data models for incoming data from web scraper. Add new requirements.txt - with new libraries

---
+++ Laboratory_Work_2_HTTP_Server_CRUD/database/connection.py
@@ -0,0 +1,34 @@
+import os
+
+from dotenv import load_dotenv
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Get the database connection URL from the environment variables
+DATABASE_CONNECTION_URL = os.getenv("DATABASE_CONNECTION_URL")
+
+# Create the database engine using the connection URL - to Docker container
+# Engine is factory that can create new database connections for us, which also holds onto connections inside of a
+# Connection Pool for fast reuse
+engine = create_engine(DATABASE_CONNECTION_URL)
+
+# Create a session object - Session establishes all conversations with the database, it will permit me to use the ORM
+# framework to define the schema of the database and query it not using SQL queries, but using ORM methods
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Create a base class for the ORM models - Base class will be used to create the models of the database
+Base = declarative_base()
+
+
+# Function to get the database connection
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
