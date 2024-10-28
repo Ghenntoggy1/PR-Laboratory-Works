e4e6dea5b26257b423c261347a9477c493f79a97
Ghenntoggy1 <roman.gusev@isa.utm.md>
2024-10-28 22:35:15 +0200

    feature: Implement Websockets with Chat Rooms via separate server on different port. Modify main file to run both servers in separate threads.

---
+++ Laboratory_Work_2_HTTP_Server_CRUD/Websocket/websocket_client.js
@@ -0,0 +1,74 @@
+let websocket;
+let room;
+let username;
+
+function joinRoom() {
+    const room = document.getElementById("roomInput").value;
+    const username = document.getElementById("username").value;
+    console.log(room, username);
+    if (!room || !username) {
+        alert("Please enter a room and username");
+        logMessage("Please enter a room and username");
+        return;
+    }
+
+
+    websocket = new WebSocket("ws://localhost:8001");
+    let connectionEstablished = false;
+
+    websocket.onopen = () => {
+        connectionEstablished = true;
+        websocket.send(JSON.stringify({ type: "join", username: username, room_id: room }));
+        document.getElementById("connect").style.display = "none";
+        document.getElementById("disconnect").style.display = "block";
+    };
+
+    websocket.onmessage = (event) => {
+        const data = JSON.parse(event.data);
+        if (data.username && data.username === "System") {
+            logMessage(`${data.username}: ${data.message}`);
+        } else {
+            logMessage(`Client ${data.username}: ${data.message}`);
+        }
+    };
+
+    websocket.onclose = () => {
+        const chatbox = document.getElementById("chatbox");
+        chatbox.innerHTML = "";
+        if (!connectionEstablished) {
+            logMessage("Unable to connect to server");
+        }
+        disconnect(username);
+        logMessage("Disconnected from server");
+        document.getElementById("connect").style.display = "block";
+        document.getElementById("disconnect").style.display = "none";
+    };
+}
+
+function sendMessage() {
+    const message = document.getElementById("messageInput").value;
+    if (websocket.readyState !== WebSocket.OPEN) {
+        alert("Not connected to server");
+        logMessage("Not connected to server");
+        return;
+    }
+    websocket.send(JSON.stringify({ type: "message", username: username, message: message }));
+    document.getElementById("messageInput").value = "";
+}
+function disconnect(username) {
+    websocket.send(JSON.stringify({ type: "quit", username: username }));
+    websocket.close();
+}
+
+function logMessage(message) {
+    const chatbox = document.getElementById("chatbox");
+    chatbox.innerHTML += message + "<br>";
+    chatbox.scrollTop = chatbox.scrollHeight;
+}
+
+window.addEventListener("beforeunload", () => {
+    if (websocket && websocket.readyState === WebSocket.OPEN) {
+        websocket.send(JSON.stringify({ type: "quit", username: username }));
+        websocket.close();
+    }
+});
--- Laboratory_Work_2_HTTP_Server_CRUD/main.py
+++ Laboratory_Work_2_HTTP_Server_CRUD/main.py
@@ -1,12 +1,20 @@
+import asyncio
+from threading import Thread
+
+import uvicorn
+
 from fastapi import FastAPI, Depends
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+
 from sqlalchemy import text
 from sqlalchemy.orm import Session
 
 from database.connection import get_db
 from router.prices_router import prices_router
 from router.phones_router import phones_router
-
 from models import PhoneTable, PriceTable
+from Websocket import websocket_server
 
 
 app = FastAPI()
@@ -16,6 +24,8 @@ app.include_router(phones_router)
 # Create the tables in the database
 PhoneTable.create_table()
 PriceTable.create_table()
+
+app.mount('/Websocket', StaticFiles(directory='Websocket', html=True), name='Websocket')
 
 
 @app.get('/')
@@ -30,3 +40,31 @@ def db_check(db: Session = Depends(get_db)):
         return {"message": "Database is connected"}
     except Exception as e:
         return {"error": str(e)}
+
+
+@app.get("/client")
+def get_chat_page():
+    with open("Websocket/index.html") as f:
+        return HTMLResponse(content=f.read())
+
+
+def start_websocket_server():
+    asyncio.run(websocket_server.start_server())
+
+
+def start_http_server():
+    uvicorn.run("main:app", host="localhost", port=8000)
+
+
+if __name__ == "__main__":
+    # Separate threads for websocket and http servers
+    websocket_thread = Thread(target=start_websocket_server)
+    http_thread = Thread(target=start_http_server)
+
+    # Start the threads
+    websocket_thread.start()
+    http_thread.start()
+
+    # Wait for the threads to finish
+    websocket_thread.join()
+    http_thread.join()
